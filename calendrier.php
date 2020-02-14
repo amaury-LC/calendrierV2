a0197544267683bd2e3e39b6258d8a325459c07b
amaury-LC <amaury.le.chaffotec@laposte.net>
2020-02-14 16:51:55 +0100

    commit

---
+++ calendrier.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="calendrier.css" rel="stylesheet">
+     <!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="calendrier.js"></script>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+<div class="row">
+    <div class="col s12 m2">
+      <div class="card-panel orange lighten-5" style="height : auto">
+        
+      </div>
+    </div>
+    <div class="col s12 m2">
+      <div class="card-panel orange lighten-5" style="height : auto">
+        
+      </div>
+    </div>
+    <div class="col s12 m2">
+      <div class="card-panel orange lighten-5" style="height : auto">
+        
+      </div>
+    </div>
+    <div class="col s12 m2">
+      <div class="card-panel orange lighten-5" style="height : auto">
+        
+      </div>
+    </div>
+    <div class="col s12 m2">
+      <div class="card-panel orange lighten-5" style="height : auto">
+        
+      </div>
+    </div>
+    <div class="col s12 m2">
+      <div class="card-panel orange lighten-5" style="height : auto">
+        
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+</body>
+</html>
